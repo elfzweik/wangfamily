d35a1f298f5fe575865fcc9be6367cbeaade6848
elfzweik <wzxuan@foxmail.com>
2022-09-09 03:02:45 -0400

    update

--- blogsite/settings/base.py
+++ blogsite/settings/base.py
@@ -130,8 +130,8 @@ CACHES = {
         }
     },
     'renditions': {
-        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
-        'LOCATION': '127.0.0.1:11211',
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
         'TIMEOUT': 600,
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
